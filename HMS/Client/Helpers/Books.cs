c5faf3c8f714bdef82be43d7634636e6a71dba94
simbert1 <69716955+simbert1@users.noreply.github.com>
2020-08-15 15:29:43 +0200

    Initial commit

---
+++ HMS/Client/Helpers/Books.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HMS.Client.Helpers
+{
+    public class Books
+    {
+        public string Tittle { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ HMS/Shared/Entities/Books.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Shared.Entities
+{
+    class Books
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
